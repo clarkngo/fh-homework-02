85916a279fc981b6051dd8ccd416ae7d3ad26af5
clarkngo <clarkngo@gmail.com>
2018-10-03 07:29:04 +0000

    book_classification, book_type and validations

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,38 @@
 class Book < ApplicationRecord
+  
+  book_classification = [
+   'General Works - encyclopedias',
+   'Philosophy, Psychology, Religion',
+   'History - Auxiliary Sciences',
+   'History (except American)',
+   'General U.S. History',
+   'Local U.S. History',
+   'Geography, Anthropology, Recreation',
+   'Social Sciences U',
+   'Political Science V',
+   'Law Z - Bibliography and Library Science',
+   'Education',
+   'Music',
+   'Fine Arts',
+   'Language and Literature',
+   'Science',
+   'Medicine',
+   'Agriculture',
+   'Technology',
+   'Military',
+   'Naval Science',
+   'Bibliography and Library Science'
+  ]
+
+  book_classification = [
+   'Fiction',
+   'Nonfiction'
+  ]
+
+  validates :title, presence: true
+  validates :author, presence: true
+  validates :genre, presence: true
+  validates :classification, presence: true, inclusion: { in: book_classification }
+  validates :type, presence: true, inclusion: { in: book_type }
+  validates :year, presence: true, length: { minimum: 4 }
 end
