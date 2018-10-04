2cd43972eb082e7162ddb8829df36b2bf0ca16d4
clarkngo <clarkngo@gmail.com>
2018-10-04 02:53:57 +0000

    add select function in edit and new html, remove select option in controller

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,34 +1,5 @@
 class Book < ApplicationRecord
-  
-  book_classification = [
-   'General Works - encyclopedias',
-   'Philosophy, Psychology, Religion',
-   'History - Auxiliary Sciences',
-   'History (except American)',
-   'General U.S. History',
-   'Local U.S. History',
-   'Geography, Anthropology, Recreation',
-   'Social Sciences U',
-   'Political Science V',
-   'Law Z - Bibliography and Library Science',
-   'Education',
-   'Music',
-   'Fine Arts',
-   'Language and Literature',
-   'Science',
-   'Medicine',
-   'Agriculture',
-   'Technology',
-   'Military',
-   'Naval Science',
-   'Bibliography and Library Science'
-  ]
-
-  book_type = [
-   'Fiction',
-   'Nonfiction'
-  ]
-
+ 
   validates :title, presence: true
   validates :author, presence: true
   validates :genre, presence: true
