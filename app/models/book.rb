f54a32ed30499900d096b35f3d31c5a8d45a7b1b
clarkngo <clarkngo@gmail.com>
2018-10-04 05:00:26 +0000

    search by title

--- app/models/book.rb
+++ app/models/book.rb
@@ -6,4 +6,9 @@ class Book < ApplicationRecord
   validates :classification, presence: true
   validates :type_book, presence: true
   validates :year, presence: true, length: { minimum: 4 }
+
+  def self.search_by(search_term)
+    where("LOWER(title) LIKE :search_term", 
+    search_term: "%#{search_term.downcase}%")
+  end
 end
