03c5265f3d8e978ff348f8639210b1e4953d08b0
clarkngo <clarkngo@gmail.com>
2018-10-08 03:04:24 +0000

    created full_name method on Author model

--- app/controllers/authors_controller.rb
+++ app/controllers/authors_controller.rb
@@ -1,2 +1,24 @@
 class AuthorsController < ApplicationController
+
+  def new 
+    @author = Author.new
+  end
+  
+  def create
+    @author = Author.create(author_params)
+    if @book.invalid?
+      flash[:error] = '<strong>Could not save</strong> the data you entered is invalid.'
+    end
+    redirect_to root_path
+  end
+
+  private 
+
+  def author_params
+    params.require(:author).permit(:first_name, :last_name)
+  end
+
+  def full_name
+    @author.first_name
+  end
 end
--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,9 @@
 class Author < ApplicationRecord
+  
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+
+  def full_name
+    "#{first_name} #{last_name}"
+  end
 end
