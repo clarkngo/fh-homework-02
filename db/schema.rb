1294a2e3aab55759eaaf4d8c25509cc1cf238a64
clarkngo <clarkngo@gmail.com>
2018-10-08 03:14:58 +0000

    removed author column in books table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_08_022846) do
+ActiveRecord::Schema.define(version: 2018_10_08_030930) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,7 +25,6 @@ ActiveRecord::Schema.define(version: 2018_10_08_022846) do
 
   create_table "books", force: :cascade do |t|
     t.string "title"
-    t.string "author"
     t.string "genre"
     t.string "classification"
     t.string "type_book"
