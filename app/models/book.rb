e0721be8b2184906838a59e78eda1b4a6af19561
clarkngo <clarkngo@gmail.com>
2018-10-09 21:32:42 +0000

    classifications and type_books

--- app/models/book.rb
+++ app/models/book.rb
@@ -14,4 +14,37 @@ class Book < ApplicationRecord
     search_term: "%#{search_term.downcase}%")
   end
 
+  def self.classifications
+    [
+      'General Works - encyclopedias',
+      'Philosophy, Psychology, Religion',
+      'History - Auxiliary Sciences',
+      'History (except American)',
+      'General U.S. History',
+      'Local U.S. History',
+      'Geography, Anthropology, Recreation',
+      'Social Sciences U',
+      'Political Science V',
+      'Law Z - Bibliography and Library Science',
+      'Education',
+      'Music',
+      'Fine Arts',
+      'Language and Literature',
+      'Science',
+      'Medicine',
+      'Agriculture',
+      'Technology',
+      'Military',
+      'Naval Science',
+      'Bibliography and Library Science'
+    ]
+  end
+
+  def self.type_books
+    [
+      'Fiction',
+      'Nonfiction'
+    ]  
+  end
+  
 end
