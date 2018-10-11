e411fa3d7fcfa08e277eefce0c94f97157f55d59
clarkngo <clarkngo@gmail.com>
2018-10-11 04:19:16 +0000

    updated authorship controller

--- app/controllers/authorships_controller.rb
+++ app/controllers/authorships_controller.rb
@@ -1,2 +1,17 @@
 class AuthorshipsController < ApplicationController
+
+
+  def new 
+    @authorship = Authorship.new
+  end
+  
+  def create
+    @authorship = Authorship.create(authorship_params)
+
+    redirect_to root_path
+  end
+
+  def authorship_params
+    params.require(:authorship).permit(:book_id, :author_id)
+  end
 end
