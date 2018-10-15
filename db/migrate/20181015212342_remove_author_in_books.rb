923a144b45812db19f5c28c1709830db7cbc76e9
Conrad Taylor <conradwt@gmail.com>
2018-10-15 15:52:45 -0700

    Fix migrations, schema, and ruby version set.

---
+++ db/migrate/20181015212342_remove_author_in_books.rb
@@ -0,0 +1,5 @@
+class RemoveAuthorInBooks < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :books, :author
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_15_064715) do
+ActiveRecord::Schema.define(version: 2018_10_15_214021) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -34,9 +34,9 @@ ActiveRecord::Schema.define(version: 2018_10_15_064715) do
     t.string "classification"
     t.string "book_type"
     t.integer "year"
-    t.string "sub_title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "sub_title"
   end
 
 end
