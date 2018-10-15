e8b510cb5b4091d1753aff82166c48f831f4d268
clarkngo <clarkngo@gmail.com>
2018-10-15 06:40:03 +0000

    re doing error

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,75 +1,78 @@
 class Book < ApplicationRecord
-  has_many :authorships
-  has_many :authors, :through => :authorships
- 
-  validates :title, presence: true
-  validates :genre, presence: true
-  validates :classification, presence: true
-  validates :book_type, presence: true
-  validates :year, presence: true, length: { minimum: 4 }
-
-  def self.search(keyword)
-    if keyword
-      Book.where(
-        query_string, 
-        "%#{keyword}", "%#{keyword}",
-        "%#{keyword}", "%#{keyword}",
-        "%#{keyword}", "%#{keyword}", 
-        "%#{keyword}", "%#{keyword}"
-        )
-    else 
-      Book.all
+  class Book < ApplicationRecord
+    has_many :authorships
+    has_many :authors, :through => :authorships
+   
+    validates :title, presence: true
+    validates :genre, presence: true
+    validates :classification, presence: true
+    validates :book_type, presence: true
+    validates :year, presence: true, length: { minimum: 4 }
+  
+    def self.search(keyword)
+      if keyword
+        Book.where(
+          query_string, 
+          "%#{keyword}", "%#{keyword}",
+          "%#{keyword}", "%#{keyword}",
+          "%#{keyword}", "%#{keyword}", 
+          "%#{keyword}", "%#{keyword}"
+          )
+      else 
+        Book.all
+      end
     end
-  end
-
-  def self.query_string
-    fields = ''
-    fields += 'title ILIKE ?'
-    fields += ' OR '
-    fields += 'sub_title ILIKE ?',
-    fields += 'classification ILIKE ?'
-    fields += ' OR '
-    fields += 'genre ILIKE ?',
-    fields += 'book_type ILIKE ?'
-    fields += ' OR '
-    fields += 'year ILIKE ?',
-    fields += 'first_name ILIKE ?'
-    fields += ' OR '
-    fields += 'last_name ILIKE ?'
-    fields
-  end
-
-  def self.classifications
-    [
-      'General Works - encyclopedias',
-      'Philosophy, Psychology, Religion',
-      'History - Auxiliary Sciences',
-      'History (except American)',
-      'General U.S. History',
-      'Local U.S. History',
-      'Geography, Anthropology, Recreation',
-      'Social Sciences U',
-      'Political Science V',
-      'Law Z - Bibliography and Library Science',
-      'Education',
-      'Music',
-      'Fine Arts',
-      'Language and Literature',
-      'Science',
-      'Medicine',
-      'Agriculture',
-      'Technology',
-      'Military',
-      'Naval Science',
-      'Bibliography and Library Science'
-    ]
-  end
-
-  def self.book_types
-    [
-      'Fiction',
-      'Nonfiction'
-    ]  
+  
+    def self.query_string
+      fields = ''
+      fields += 'title ILIKE ?'
+      fields += ' OR '
+      fields += 'sub_title ILIKE ?',
+      fields += 'classification ILIKE ?'
+      fields += ' OR '
+      fields += 'genre ILIKE ?',
+      fields += 'book_type ILIKE ?'
+      fields += ' OR '
+      fields += 'year ILIKE ?',
+      fields += 'first_name ILIKE ?'
+      fields += ' OR '
+      fields += 'last_name ILIKE ?'
+      fields
+    end
+  
+    def self.classifications
+      [
+        'General Works - encyclopedias',
+        'Philosophy, Psychology, Religion',
+        'History - Auxiliary Sciences',
+        'History (except American)',
+        'General U.S. History',
+        'Local U.S. History',
+        'Geography, Anthropology, Recreation',
+        'Social Sciences U',
+        'Political Science V',
+        'Law Z - Bibliography and Library Science',
+        'Education',
+        'Music',
+        'Fine Arts',
+        'Language and Literature',
+        'Science',
+        'Medicine',
+        'Agriculture',
+        'Technology',
+        'Military',
+        'Naval Science',
+        'Bibliography and Library Science'
+      ]
+    end
+  
+    def self.book_types
+      [
+        'Fiction',
+        'Nonfiction'
+      ]  
+    end
+    
   end
   
 end
--- db/migrate/20181008032341_add_sub_title_to_books.rb
+++
@@ -1,6 +0,0 @@
-class AddSubTitleToBooks < ActiveRecord::Migration[5.2]
-  def change
-    # add / remove :table_name, :field/column_name, :data_type
-    add_column :books, :sub_title, :string
-  end
-end
