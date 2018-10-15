6f2c005a20a3f641389dc95c94fd7459962537db
clarkngo <clarkngo@gmail.com>
2018-10-15 06:55:31 +0000

    not fixed

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,2 +1,45 @@
 class Book < ApplicationRecord
+  validates :title, presence: true
+  validates :genre, presence: true
+  validates :classification, presence: true
+  validates :book_type, presence: true
+  validates :year, presence: true
+
+  def self.search(search_by, search_term)
+    where("LOWER(#{search_by}) LIKE :search_term", 
+    search_term: "%#{search_term.downcase}%")
+  end
+
+  def self.classifications
+    [
+      'General Works - encyclopedias',
+      'Philosophy, Psychology, Religion',
+      'History - Auxiliary Sciences',
+      'History (except American)',
+      'General U.S. History',
+      'Local U.S. History',
+      'Geography, Anthropology, Recreation',
+      'Social Sciences U',
+      'Political Science V',
+      'Law Z - Bibliography and Library Science',
+      'Education',
+      'Music',
+      'Fine Arts',
+      'Language and Literature',
+      'Science',
+      'Medicine',
+      'Agriculture',
+      'Technology',
+      'Military',
+      'Naval Science',
+      'Bibliography and Library Science'
+    ]
+  end
+
+  def self.book_types
+    [
+      'Fiction',
+      'Nonfiction'
+    ]  
+  end
 end
